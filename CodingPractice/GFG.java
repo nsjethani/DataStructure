2348f0f0e32a6965c9b166be6259b20a6ecf96e3
nsjethani <neha.jethani6627@gmail.com>
2018-01-19 12:32:02 -0800

    Subarray with given sum

---
+++ CodingPractice/GFG.java
@@ -0,0 +1,54 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+class GFG {
+	public static void main (String[] args) throws IOException {
+		//code
+		InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader in = new BufferedReader(reader);
+        GFG g1=new GFG();
+        String line;
+        line=in.readLine();
+        String[] sum,input,a1;
+        int test=Integer.parseInt(line);
+        for(int a=0;a<test;a++)
+        {
+            line=in.readLine();
+            sum=line.split(" ");
+            line=in.readLine();
+            a1=line.split(" ");
+            g1.findSub(a1,Integer.parseInt(sum[1]));
+        }
+	}
+	
+	public void findSub(String arr[],int sum)
+    {
+        int begin=0,end=0,s,j;
+        for(int i=0;i<arr.length;i++)
+        {
+            j=i+1; s=Integer.parseInt(arr[i]);
+            if(j!=arr.length)
+            {
+                while(s<sum)
+                {
+                    s+=Integer.parseInt(arr[j]);
+                    j++;
+                }
+            }
+
+            if(s==sum)
+            {
+                begin=i+1; end=j;
+                if(begin==0)
+                    System.out.println("-1");
+                else
+                    System.out.println(begin+" "+end);
+                break;
+            }
+        }
+    }
+}
